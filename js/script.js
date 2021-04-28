f9fc1a5ac91bfb81cb91828a08c2ffb31248642b
Jalalajlan <55819195+Jalalajlan@users.noreply.github.com>
2021-04-28 22:07:39 +0800

    Event Scheduler layout design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log(document);
+
+var d = new Date();
+var d2 = new Date();
+
+var same = d.getTime() === d2.getTime();
+console.log(same);
+console.log(d.getTime());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getMonth() + 1);
+console.log(d.getFullYear());
